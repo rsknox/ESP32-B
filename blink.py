8d31be54fa052c05b40e2ddec87edcc51681d593
R.S <rsknox@gmail.com>
2024-06-05 16:30:39 -0500

    Blink at one-half second cycle

---
+++ blink.py
@@ -0,0 +1,11 @@
+import machine
+import time
+led = machine.Pin(4, machine.Pin.OUT)
+led2 = machine.Pin(2, machine.Pin.OUT)
+while True:
+    led.value(1)
+    led2.value(0)
+    time.sleep(.5)
+    led.value(0)
+    led2.value(1)
+    time.sleep(.5)
